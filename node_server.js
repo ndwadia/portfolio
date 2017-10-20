238b3c3c56576a149c1c78f8d13c6411c3ff24e3
Neville <ndwadia@gmail.com>
2017-10-20 21:14:49 +0530

    Changed path for static files

--- node_server.js
+++ node_server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('./static')).
+app.use('/', express.static('./')).
     use('/images', express.static( './images')).
     use('/lib', express.static( './lib'));
 app.listen(80);
