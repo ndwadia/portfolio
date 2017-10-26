73b2267114a7822a00628648c00059da66339fdc
Neville <ndwadia@gmail.com>
2017-10-26 16:36:13 +0530

    Adding data table component

--- routes.js
+++ routes.js
@@ -7,6 +7,8 @@ module.exports = function(app) {
   var chart4 = require('./controllers/chart4_controller');
   var chart5 = require('./controllers/chart5_controller');
   var chart6 = require('./controllers/chart6_controller');
+  var bubbleChart = require('./controllers/bubbleChart_controller');
+  var dataTable = require('./controllers/dataTable_controller');
 
   app.use('/', express.static('./'));
   app.get('/chart1', chart1.getData);
@@ -15,4 +17,6 @@ module.exports = function(app) {
   app.get('/chart4', chart4.getData);
   app.get('/chart5', chart5.getData);
   app.get('/chart6', chart6.getData);
+  app.get('/bubbleChart', bubbleChart.getData);
+  app.get('/dataTable', dataTable.getData);
 };
