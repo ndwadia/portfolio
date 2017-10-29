7df29755ffa80d1435893d4a943079fedf8e36db
Neville <ndwadia@gmail.com>
2017-10-29 22:52:32 +0530

    Changes to read Mongo url from config.json

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./db');
-
+var fs = require('fs');
+var path = require('path');
+var nconf = require('nconf');
 var app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 require('./routes')(app);
-
 // Connect to Mongo on start
-var uri = process.env.MONGODB_URI;
+nconf.file({ file: path.join(__dirname, 'config.json') });
+var uri = nconf.get('MONGO_URL');
 db.connect(uri, function(err) {
   if (err) {
-    console.log('Unable to connect to Mongo.');
+    console.log('Unable to connect to Mongo');
     process.exit(1);
   } else {
-    app.listen(8080, function() {
-    });
+    app.listen(8080, function() {});
   }
 });
