f32b55c28da0b3e7414c652b4ea14e5b07f7d3db
Neville <ndwadia@gmail.com>
2017-10-29 17:09:50 +0530

    Renamed to app.js & changed port to 8080

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ db.connect(uri, function(err) {
     console.log('Unable to connect to Mongo.');
     process.exit(1);
   } else {
-    app.listen(3000, function() {
-      console.log('Listening on port 3000...');
+    app.listen(8080, function() {
+      // console.log('Listening on port 3000...');
     });
   }
 });
